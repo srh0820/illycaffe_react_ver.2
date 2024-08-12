4d2428c6fb8dcd2af2af3e140d34ac48ffc47bb3
srh0820 <srh96820@gmail.com>
2024-08-12 17:38:03 +0900

    [0.3.0] Main section 1차 작업 완료

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Main() {
+import mainscss from './scss/main.module.scss';
+
+const Main = () => {
     return (
-        <></>
-    )
-}
+        <section className={mainscss.main}>
+            <div className={`${mainscss.mainBanner} d-flex align-items-center`}>
+                <div className={mainscss.bgImg}>
+                    <div className={mainscss.mainTextbox}>
+                        <p className={`${mainscss.mainText1} ms-2`}>
+                            QUALITY <br />
+                            LOVES <br />
+                            DETAILS
+                        </p>
+                        <p className={`${mainscss.mainText2} ms-2`}>
+                            일리커피는 아라비카 원두만을 사용하여 독보적인 블렌딩으로 탄생했습니다
+                        </p>
+                        <Link to="/" className={`${mainscss.btn} ms-2`}>
+                            자세히보기
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
 
-export default Main
+export default Main;
