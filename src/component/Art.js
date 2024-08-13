40f672c838287419bd4362046f8f4cf58363e496
srh0820 <srh96820@gmail.com>
2024-08-13 15:10:30 +0900

    [0.6.0] Art section 1차 작업 완료

--- src/component/Art.js
+++ src/component/Art.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-function Art() {
+import artscss from './scss/art.module.scss';
+
+const Art = () => {
     return (
-        <></>
-    )
-}
+        <section className={artscss.art}>
+            <div className={artscss.box}>
+                <div className={`${artscss.videoArea} d-flex justify-content-center`}>
+                    <img src="/img/artColletion.jpg" alt="Illy Art Collection Video" />
+                    <button className={artscss.playButton} aria-label="Play Video"></button>
+                </div>
+                <div className={artscss.textArea}>
+                    <h2 className="mb-3">ILLY ART COLLECTION</h2>
+                    <p className={`${artscss.p1} mb-3`}>JUDY CHICAGO - 4 ESPRESSO</p>
+                    <p className={artscss.p2}>
+                        1960년대 미국 미술계에 등장한 가장 영향력 있는 예술가 <br />
+                        주디 시카고의 70년대 작품 속 감각적인 움직임을 컵의 우아한 형태로 표현
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
 
-export default Art
+export default Art;
