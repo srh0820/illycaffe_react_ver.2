75cfb2b8812e25414c766dd4be36112c2376c03d
srh0820 <srh96820@gmail.com>
2024-08-15 02:46:34 +0900

    [0.12.1] Company section @keyframes

--- src/component/Company.js
+++ src/component/Company.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import cpscss from './scss/company.module.scss';
 import Comswiper from './company/Comswiper';
 
 function Company() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const element = document.querySelector(`.comtext`);
+
+        if (element) {
+            const observer = new IntersectionObserver(
+                ([entry]) => {
+                    setIsVisible(entry.isIntersecting);
+                },
+                {
+                    root: null,
+                    rootMargin: '0px',
+                    threshold: 0.1 // 10%
+                }
+            );
+
+            observer.observe(element);
+
+            return () => {
+                observer.unobserve(element);
+            };
+        }
+    }, []);
+
     return (
         <section className={cpscss.company}>
             <div className={cpscss.comdiv}>
-                <div className={cpscss.comtext}>
+                <div className={`${cpscss.comtext} comtext ${isVisible ? cpscss.visible : ''}`}>
                     <h2>
                         일리는 혁신과 직관, 전문성으로 탄생된 아주 특별한 <br />
                         커피 브랜드입니다. 일리만의 맛과 느낌, 아로마 향이 있습니다. <br />
@@ -19,8 +44,7 @@ function Company() {
                     </Link>
                 </div>
 
-                <Comswiper></Comswiper>
-                
+                <Comswiper />
             </div>
         </section>
     );
