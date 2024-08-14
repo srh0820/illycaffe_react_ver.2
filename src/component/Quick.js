0e4071a055a40475d2991f9a0d53d2bf88af8597
srh0820 <srh96820@gmail.com>
2024-08-14 18:15:39 +0900

    [0.10.0] Quick section 1차 작업 완료

--- src/component/Quick.js
+++ src/component/Quick.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-function Quick() {
-    return (
-        <></>
-    )
-}
+import Quickscss from './scss/quick.module.scss';
 
-export default Quick
+import { ReactComponent as Top } from '../img/quicktop.svg';
+
+const Quick = () => (
+    <section className={Quickscss.quick}>
+        <ul>
+            <li>
+                <a href="#top">
+                    <Top width="45" height="45" />
+                    <span className="visually-hidden">위로</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+);
+
+export default Quick;
