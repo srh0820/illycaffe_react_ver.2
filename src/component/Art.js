caa5d31867cae11442903df7dbd1893e6d963dfc
srh0820 <srh96820@gmail.com>
2024-08-15 22:59:43 +0900

    [0.12.2] Art section react-youtube 사용해서 동영상 연동

--- src/component/Art.js
+++ src/component/Art.js
@@ -1,14 +1,38 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import YouTube from 'react-youtube';
 import artscss from './scss/art.module.scss';
 
 const Art = () => {
+    const [showVideo, setShowVideo] = useState(false);
+
+    const handlePlayButtonClick = () => {
+        setShowVideo(true);
+    };
+
+    const handleVideoEnd = () => {
+        setShowVideo(false); // 동영상이 끝나면 썸네일 이미지로 돌아감
+    };
+
+    const opts = {
+        height: '530',
+        width: '1400',
+        playerVars: {
+            autoplay: 1, 
+        },
+    };
+
     return (
         <section className={artscss.art}>
             <div className={artscss.box}>
                 <div className={`${artscss.videoArea} d-flex justify-content-center`}>
-                    <img src="/img/artColletion.jpg" alt="Illy Art Collection Video" />
-                    <button className={artscss.playButton} aria-label="Play Video"></button>
+                    {!showVideo ? (
+                        <div className={artscss.thumbnail} onClick={handlePlayButtonClick}>
+                            <img src="/img/artcolletion.jpg" alt="Custom Thumbnail" />
+                            <button className={artscss.playButton} aria-label="Play Video"></button>
+                        </div>
+                    ) : (
+                        <YouTube videoId="k9X1wxnneMs" opts={opts} onEnd={handleVideoEnd} />
+                    )}
                 </div>
                 <div className={artscss.textArea}>
                     <h2 className="mb-3">ILLY ART COLLECTION</h2>
