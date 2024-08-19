29883f97664ae14350054fc8627b30a6841081bd
srh0820 <srh96820@gmail.com>
2024-08-20 01:39:04 +0900

    [1.2.1] Product section 반응형 작업중 - 스와이퍼

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,48 +1,84 @@
 import React, { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import pdscss from './scss/product.module.scss';
 import products from '../json/productdata.json'; 
 
 function Product() {
-    const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1024);
+    const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1290);
+    const [isSlide, setIsSlide] = useState(window.innerWidth < 769);
 
     useEffect(() => {
-        const handleResize = () => setIsLargeScreen(window.innerWidth >= 1024);
-        handleResize();
+        const handleResize = () => {
+            setIsLargeScreen(window.innerWidth >= 1290);
+            setIsSlide(window.innerWidth < 769);
+        };
+
+        handleResize(); // 초기 렌더링 시 호출
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
     }, []);
+
     return (
         <section className={pdscss.product}>
-            <ul className={`${pdscss.productUl} d-flex justify-content-center mb-0`}>
-                {products.map((product, index) => (
-                    <li className={pdscss.productBox} key={index}>
-                        <div className={`${pdscss.hoverBox} d-flex`}>
-                            <div className={pdscss.textarea}>
-                                <p className={pdscss.titletext}>{product.title}</p>
-                                <p className={pdscss.subtext}>
-                                    {isLargeScreen ? (
-                                        <>
+            {isSlide ? (
+                <Swiper 
+                    slidesPerView={1} 
+                    pagination={{ clickable: true }}
+                    loop={true}
+                >
+                    {products.map((product, index) => (
+                        <SwiperSlide key={index}>
+                            <div className={pdscss.productBox}>
+                                <div className={`${pdscss.hoverBox} d-flex`}>
+                                    <div className={pdscss.textarea}>
+                                        <p className={pdscss.titletext}>{product.title}</p>
+                                        <p className={pdscss.subtext}>
                                             {product.description.split('|')[0]}<br />
                                             {product.description.split('|')[1]}
-                                        </>
-                                    ) : (
-                                        <>
-                                            {product.description2.split('|')[0]}<br />
-                                            {product.description2.split('|')[1]}
-                                        </>
-                                    )}
-                                </p>
-                            </div>
-                            <div className={pdscss.imgarea}>
-                                <div className={pdscss.prdImg}>
-                                    <img src={product.image} alt={product.alt} />
+                                        </p>
+                                    </div>
+                                    <div className={pdscss.imgarea}>
+                                        <div className={pdscss.prdImg}>
+                                            <img src={product.image} alt={product.alt} />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </li>
-                ))}
-            </ul>           
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            ) : (
+                <ul className={`${pdscss.productUl} d-flex justify-content-center mb-0`}>
+                    {products.map((product, index) => (
+                        <li className={pdscss.productBox} key={index}>
+                            <div className={`${pdscss.hoverBox} d-flex`}>
+                                <div className={pdscss.textarea}>
+                                    <p className={pdscss.titletext}>{product.title}</p>
+                                    <p className={pdscss.subtext}>
+                                        {isLargeScreen ? (
+                                            <>
+                                                {product.description.split('|')[0]}<br />
+                                                {product.description.split('|')[1]}
+                                            </>
+                                        ) : (
+                                            <>
+                                                {product.description2.split('|')[0]}<br />
+                                                {product.description2.split('|')[1]}
+                                            </>
+                                        )}
+                                    </p>
+                                </div>
+                                <div className={pdscss.imgarea}>
+                                    <div className={pdscss.prdImg}>
+                                        <img src={product.image} alt={product.alt} />
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </section>
     );
 };
