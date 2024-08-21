827a7f07d17fb0052f617f728ac82469bec5c672
srh0820 <srh96820@gmail.com>
2024-08-21 15:24:25 +0900

    [1.4.0] Art section 1차 작업 완료

--- src/component/Art.js
+++ src/component/Art.js
@@ -39,8 +39,15 @@ function Art() {
                     <h2 className="mb-3">ILLY ART COLLECTION</h2>
                     <p className={`${artscss.p1} mb-3`}>JUDY CHICAGO - 4 ESPRESSO</p>
                     <p className={artscss.p2}>
-                        1960년대 미국 미술계에 등장한 가장 영향력 있는 예술가 <br />
-                        주디 시카고의 70년대 작품 속 감각적인 움직임을 컵의 우아한 형태로 표현
+                        <span className={artscss.default_text}>
+                            미국 미술계에 등장한 가장 영향력 있는 예술가 <br />
+                            주디 시카고의 70년대 작품 속 감각적인 움직임을 컵의 우아한 형태로 표현
+                        </span> 
+                        <span className={artscss.short_text}>
+                            미국 미술계에 등장한 가장 영향력 있는 예술가 <br />
+                            주디 시카고의 70년대 작품 속 <br />
+                            감각적인 움직임을 컵의 우아한 형태로 표현
+                        </span> 
                     </p>
                 </div>
             </div>
