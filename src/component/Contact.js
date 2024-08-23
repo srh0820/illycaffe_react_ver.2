5a89191889574ebed53bbff3c7a0d34c76cb7494
srh0820 <srh96820@gmail.com>
2024-08-23 12:04:18 +0900

    [1.6.1] #Contact - 1024px ~ 320px 반응형 구현

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import cntFormscss from './scss/contactForm.module.scss';
-import FormFields from './contact/FormFields';  
+import FormFields from './contact/FormFields';
 
 function Contact() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -43,7 +57,18 @@ function Contact() {
                         />
                     </div> 
                 </div>
-                <div className={`${cntFormscss.imgArea} col-6`}></div>
+                <div className={`${cntFormscss.imgArea} col-6`}>
+                    {isMobile && (
+                        <div className={cntFormscss.overlayText}>
+                            <h2 className='mb-0'>CONTACT US</h2>
+                            <button className={cntFormscss.iconButton}>
+                                <svg className={cntFormscss.arrow} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M17.5 3.125C17.5 2.77982 17.2202 2.5 16.875 2.5H9.375C9.02982 2.5 8.75 2.77982 8.75 3.125C8.75 3.47018 9.02982 3.75 9.375 3.75H15.3661L2.68306 16.4331C2.43898 16.6771 2.43898 17.0729 2.68306 17.3169C2.92714 17.561 3.32286 17.561 3.56694 17.3169L16.25 4.63388V10.625C16.25 10.9702 16.5298 11.25 16.875 11.25C17.2202 11.25 17.5 10.9702 17.5 10.625V3.125Z" fill="white"/>
+                                </svg>
+                            </button>
+                        </div>
+                    )}
+                </div>
             </div>
         </section>
     );
