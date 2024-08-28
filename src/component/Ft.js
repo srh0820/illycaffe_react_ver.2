961cf21fb3f2898228937ff389b9fa3278827479
srh0820 <srh96820@gmail.com>
2024-08-28 12:47:45 +0900

    [1.8.0] #Ft - 모바일까지 반응형 구현

--- src/component/Ft.js
+++ src/component/Ft.js
@@ -25,15 +25,28 @@ function Ft() {
                     <li className="me-4"><Link to="/">e-SHOP</Link></li>
                 </ul>
                 <div className={`${ftscss.ftText} py-5 mb-0`}>
-                    <p>(주)큐로홀딩스<br />
-                        대표자 | 조중기    사업자등록번호 | 129-81-19361    주소 | 서울특별시 강남구 테헤란로13길 31-1 (일리빌딩)<br />
-                        대표번호 │ 1666-1282    AS접수 및 문의 │ 1:1 게시판<br />
-                        마케팅제휴 │ illycaffe@illycaffe.co.kr</p>
-                    <div className={`${ftscss.ftTextIcon} d-flex justify-content-between align-items-end`}>
-                        <p className="mb-0">Copyright(c) 2021 www.illycaffe.co.kr All rights reserved.<br />
-                            © 2021 illycaffè S.p.A. | P.IVA 00055180327 | Via Flavia 110, 34147 Trieste - Italy</p>
-                        <ul className={`${ftscss.ftIcon} d-flex justify-content-end mb-0`}>
-                            <li className="ms-2 mb-0">
+                    <p className="mb-0">
+                        <span className={ftscss.default_text}>
+                            (주)큐로홀딩스<br />
+                            대표자 | 조중기 &nbsp;&nbsp; 사업자등록번호 | 129-81-19361 &nbsp;&nbsp; 주소 | 서울특별시 강남구 테헤란로13길 31-1 (일리빌딩) <br />
+                            대표번호 │ 1666-1282 &nbsp;&nbsp; AS접수 및 문의 │ 1:1 게시판 <br />
+                            마케팅제휴 │ illycaffe@illycaffe.co.kr
+                        </span>
+                        <span className={ftscss.short_text}>
+                            (주)큐로홀딩스<br />
+                            대표자 | 조중기 &nbsp;&nbsp; 사업자등록번호 | 129-81-19361 <br />
+                            주소 | 서울특별시 강남구 테헤란로13길 31-1 (일리빌딩) <br />
+                            대표번호 │ 1666-1282 &nbsp;&nbsp; AS접수 및 문의 │ 1:1 게시판 <br />
+                            마케팅제휴 │ illycaffe@illycaffe.co.kr
+                        </span>
+                    </p>
+                    <div className={`${ftscss.ftTextIcon} d-flex justify-content-between`}>
+                        <p className="mb-0">
+                            Copyright(c) 2021 <span className={`${ftscss.br} d-none`}><br /></span> www.illycaffe.co.kr All rights reserved. <br />
+                            © 2021 illycaffè S.p.A. <span> | P.IVA 00055180327 | Via Flavia 110, 34147 Trieste - Italy </span> 
+                        </p>
+                        <ul className={`${ftscss.ftIcon} d-flex align-items-center justify-content-end mb-0`}>
+                            <li className="mb-0">
                                 <a href="https://www.facebook.com/illykorea.official" target="_blank" rel="noopener noreferrer">
                                     <img src={facebookIcon} alt="일리 페이스북" />
                                 </a>
