998540e02492956a0c4c2da5ebec05d134f55e3b
srh0820 <srh96820@gmail.com>
2024-09-03 09:57:27 +0900

    [1.10.0] #Quick - 스크롤이벤트 구현

--- src/component/Quick.js
+++ src/component/Quick.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Quickscss from './scss/quick.module.scss';
 
 import { ReactComponent as Top } from '../img/quicktop.svg';
 
-const Quick = () => (
-    <section className={Quickscss.quick}>
-        <ul>
-            <li>
-                <a href="#top">
-                    <Top width="45" height="45" />
-                    <span className="visually-hidden">위로</span>
-                </a>
-            </li>
-        </ul>
-    </section>
-);
+const Quick = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.scrollY > 200) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility);
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility);
+        };
+    }, []);
+
+    return (
+        <section className={`${Quickscss.quick} ${isVisible ? Quickscss.visible : ''}`}>
+            <ul>
+                <li>
+                    <a href="#top">
+                        <Top width="45" height="45" />
+                        <span className="visually-hidden">위로</span>
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+};
 
 export default Quick;
