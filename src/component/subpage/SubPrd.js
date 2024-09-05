71def1dfc0c090389044493c4c24042cd73b6090
srh0820 <srh96820@gmail.com>
2024-09-05 18:20:22 +0900

    [2.2.2] #SubPrd - 레이아웃 구현중 중간 커밋

--- src/component/subpage/SubPrd.js
+++ src/component/subpage/SubPrd.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import subprdscss from './scss/subPrd.module.scss';
+import subprddata from './json/Subprd.json'
+
 function SubPrd() {
     return (
-        <>
-        </>
+        <section className={subprdscss.subprd}>
+            <div className='container-1240'>
+                <p className={subprdscss.p}> 관련 상품 </p>
+                <ul className={subprdscss.prdlist}>
+                    {subprddata.map((coffee, index) => (
+                    <li key={index} className={subprdscss.coffee_item}>
+                        <img src={coffee.image} alt={coffee.alt} />
+                        <p className='mb-0'>{coffee.title}</p>
+                        <p className='mb-0'>[{coffee.origin}]</p>
+                    </li>
+                ))}
+          </ul>
+            </div>
+        </section>
     )
 }
 
