5a6691ec948e19e2b77beea92fed8c68cabba4a7
srh0820 <srh96820@gmail.com>
2024-09-06 16:40:49 +0900

    [2.3.4] line 스타일 컴포넌트 생성

---
+++ src/component/styled/LineGray.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const LineGray = styled.line`
+    display: block;
+    border: 2px solid #C8C8C8;
+`
+
+export default LineGray;
---
+++ src/component/styled/LineThinGray.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const LineThinGray = styled.line`
+    display: block;
+    border: 1px solid #C8C8C8;
+`
+
+export default LineThinGray;
