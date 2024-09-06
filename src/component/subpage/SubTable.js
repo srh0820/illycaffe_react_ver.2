f569a401e25d4b87b4da5791ae30facc498b6cdf
srh0820 <srh96820@gmail.com>
2024-09-06 16:07:48 +0900

    [2.3.2] #SubTable - map, table 이용해서 컴포넌트 HTML + CSS 구현

--- src/component/subpage/SubTable.js
+++ src/component/subpage/SubTable.js
@@ -1,10 +1,35 @@
 import React from 'react'
 
 import subtablescss from './scss/subTable.module.scss';
+import subtabledata from './json/SubTable.json'
 
 function SubTable() {
     return (
-        <></>   
+        <section className={subtablescss.subtable}>
+            <div className="container-1240">
+                <p>상품 필수 정보</p>
+                <div className={`${subtablescss.tablediv} d-flex`}>
+                    {subtabledata.map((table, index) => (
+                        <table key={index} className={subtablescss.table}>
+                            <thead className='d-none'>
+                                <tr>
+                                    <th>항목</th>
+                                    <th>값</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {Object.entries(table).map(([key, value], idx) => (
+                                    <tr key={idx}>
+                                        <th>{key}</th>
+                                        <td>{value}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    ))}
+                </div>
+            </div>
+        </section>
     )
 }
 
