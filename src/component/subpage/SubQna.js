378665ec114dd418604089d80a61d37eaaa12fc9
srh0820 <srh96820@gmail.com>
2024-09-10 11:53:29 +0900

    [2.16.0] #SubQna - 모바일까지 반응형 구현

--- src/component/subpage/SubQna.js
+++ src/component/subpage/SubQna.js
@@ -6,14 +6,15 @@ import Linethingray from '../styled/LineThinGray'
 
 function SubQna() {
     return (
-        <section id='SubQna' className={subqnascss.SubQna}>
-            <div className='container-1240'>
+        <section id='SubQna' className={subqnascss.subqna}>
+            <div className={`${subqnascss.innerdiv} container-1240`}>
                 <p className={subqnascss.title}> 상품문의 </p>
                 <div className={`${subqnascss.btndiv} d-flex justify-content-between`}>
                     <p>
                         상품에 대한 문의를 남기는 공간입니다. <br />
-                        해당 게시판의 성격과 다른 글은 사전동의 없이 담당 게시판으로 이동될 수 있습니다.
+                        <span className={subqnascss.spanhidden2}> 해당 게시판의 성격과 다른 글은 <span className={subqnascss.spanhidden1}> 사전동의 없이 담당 게시판으로 </span> 이동될 수 있습니다.</span>
                     </p>
+                    <p className={subqnascss.textshort}> 상품에 대한 문의를 남겨주세요. </p>
                     <button> 문의하기 </button>
                 </div>
                 <Linegray></Linegray>
@@ -30,23 +31,30 @@ function SubQna() {
                     <tbody>
                         <tr>
                             <td className={subqnascss.textred}>공지</td>
-                            <td>프로모션 상품 운영</td>
+                            <td className={subqnascss.textbold}>프로모션 상품 운영</td>
                             <td>관리자</td>
-                            <td>2021.11.03</td>
+                            <td>2022.11.03</td>
                             <td>답변완료</td>
                         </tr>
                         <tr>
                             <td className={subqnascss.textred}>공지</td>
-                            <td>커피 캡슐 안내 (문의 전 확인 부탁드립니다.)</td>
+                            <td className={subqnascss.textbold}>커피 캡슐 안내 (문의 전 확인 부탁드립니다.)</td>
                             <td>관리자</td>
-                            <td>2020.04.07</td>
+                            <td>2022.04.07</td>
+                            <td>답변완료</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>주문 당일 배송 출발 가능한가요?</td>
+                            <td>yu****</td>
+                            <td>2023.01.04</td>
                             <td>답변완료</td>
                         </tr>
                         <tr>
                             <td>1</td>
                             <td>유통기한 문의</td>
                             <td>le****</td>
-                            <td>2022.10.20</td>
+                            <td>2022.12.20</td>
                             <td>답변완료</td>
                         </tr>
                     </tbody>
