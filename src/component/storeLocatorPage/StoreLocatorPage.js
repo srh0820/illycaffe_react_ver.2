0076568a1d4944d411f4586077ae15df1b60830b
srh0820 <srh96820@gmail.com>
2024-09-10 17:43:00 +0900

    [2.18.1] #StoreLocatorPage - 상단 레이아웃 구현

--- src/component/storeLocatorPage/StoreLocatorPage.js
+++ src/component/storeLocatorPage/StoreLocatorPage.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import storelocatorscss from './scss/storeLocator.module.scss'
 
 function  StoreLocatorPage() {
     return (
         <section className={storelocatorscss.storeLocator}>
-            페이지 준비중입니다.
+            <div className={storelocatorscss.infodiv}>
+                <h2>illy Caffè</h2>
+                <p> 프리미엄 커피 브랜드 <br />
+                    일리 카페를 가까운 매장에서 만나보세요.</p>
+                <ul>
+                    <li>
+                        <Link to="/">고객의 소리</Link>
+                    </li>
+                    <li>
+                        <Link to="/">지속 가능성</Link>
+                    </li>
+                    <li>
+                        <Link to="/">제휴 · 제안</Link>
+                    </li>
+                </ul>
+            </div>
+
         </section>
     );
 };
