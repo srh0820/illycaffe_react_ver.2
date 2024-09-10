f36355707408310aa623e0154364526114b333d4
srh0820 <srh96820@gmail.com>
2024-09-10 10:24:00 +0900

    [2.15.0] #SubReview - 모바일까지 반응형 구현

--- src/component/subpage/SubReview.js
+++ src/component/subpage/SubReview.js
@@ -7,24 +7,24 @@ import Linethingray from '../styled/LineThinGray'
 function SubReview() {
     return (
         <section id='SubReview' className={subreviewscss.SubReview}>
-            <div className='container-1240'>
+            <div className={`${subreviewscss.innerdiv} container-1240`}>
                 <div className='d-flex justify-content-between'>
                     <p className={subreviewscss.title}> 후기 </p>
                     <ul className='d-flex justify-content-between'>
                         <li> 포토리뷰 </li>
-                        <li className='ms-3'> 전체리뷰 </li>
-                        <li className='ms-3'> 제품별리뷰 </li>
+                        <li className='ms-2'> 전체리뷰 </li>
+                        <li className='ms-2'> 제품별리뷰 </li>
                     </ul>
                 </div>
                 <Linegray></Linegray>
                 <ul className={`${subreviewscss.ul2} d-flex justify-content-end`}>
                     <li> 추천순 </li>
-                    <li className='ms-3'> 최근등록순 </li>
+                    <li className='ms-2'> 최근등록순 </li>
                 </ul>
                 <div className=''>
                     <p>
                         이 상품의 첫번째 리뷰를 작성해보세요. <br />
-                        리뷰 작성시 최대 1,500 point를 드립니다.
+                        리뷰 작성시 <span>최대 3,000 point</span>를 드립니다.
                     </p>
                 </div>
                 <Linethingray></Linethingray>
